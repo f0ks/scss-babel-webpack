65df5a49173794d276d2131d9a16da54de8ab3a8
Albert Khamidullin <albert.khm@gmail.com>
2019-06-18 13:35:08 +0300

    everything I need

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import './scss/styles.scss';
+import {sum} from "./libs/lib";
+console.log( sum(1,2,3,4) ); // 10
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {minimize: true}
+          }
+        ]
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              autoprefixer: {
+                browsers: ['last 2 versions']
+              },
+              plugins: () => [autoprefixer]
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {}
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    }),
+    new MiniCssExtractPlugin()
+  ],
+  watchOptions: {
+    ignored: ['/node_modules/']
+  }
+};
